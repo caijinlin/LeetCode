f73502cbcac82367dca31752e7bef0c7fcd7e886
caijinlin <1399534656@qq.com>
2019-05-29 12:01:28 +0800

    ADD 048-rotate-image solution in php

---
+++ 048-rotate-image/solution.php
@@ -0,0 +1,19 @@
+class Solution {
+
+    /**
+     * @param Integer[][] $matrix
+     * @return NULL
+     */
+    function rotate(&$matrix) {
+        $n = count($matrix[0]);
+        for ($i=0; $i<$n/2; $i++) {
+            for ($j=$i; $j<$n-$i-1; $j++) {
+                $temp = $matrix[$i][$j];
+                $matrix[$i][$j] =  $matrix[$n-$j-1][$i];
+                $matrix[$n-$j-1][$i] = $matrix[$n-$i-1][$n-$j-1];
+                $matrix[$n-$i-1][$n-$j-1] = $matrix[$j][$n-$i-1];
+                $matrix[$j][$n-$i-1] = $temp;
+            }
+        }
+    }
+}
